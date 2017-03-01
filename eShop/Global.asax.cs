eb3a7ce818d2b2f563a47884435b2398ee26a3ee
srothfus <srothfus@gmail.com>
2017-03-01 01:13:41 -0500

    Mobile View
    Adding list to mobile view and fixing removal of details from list.

--- eShop/Global.asax.cs
+++ eShop/Global.asax.cs
@@ -8,6 +8,7 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.WebPages;
 
 namespace eShop
 {
@@ -15,9 +16,14 @@ namespace eShop
     {
         protected void Application_Start()
         {
+            BundleMobileConfig.RegisterBundles(BundleTable.Bundles);
             Mapper.Initialize(c => c.AddProfile<MappingProfile>());
             GlobalConfiguration.Configure(WebApiConfig.Register);
+
             AreaRegistration.RegisterAllAreas();
+
+            var displayModes = DisplayModeProvider.Instance.Modes;
+
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
